b842bc733ff8d1da0ac8623adf1da6d3730d1ad8
Mike Gregoire <mgregoire@nvzion.com>
2021-02-26 15:31:25 -0500

    Change message

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -3,6 +3,6 @@ exports.handler = async (event, context) => {
     console.log('context:', context);
     return {
         statusCode: 200,
-        body: 'Some cool stuff here'
+        body: 'Yo, check this out…'
     };
 };
